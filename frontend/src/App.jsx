216b7e3fee974750e7abe0722314ae81fccbaefb
Jinwook-Lee9709 <wooki9709@gmail.com>
2025-10-24 14:29:55 +0900

    로그인 엔드포인트연동

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,42 @@ import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
-import { BrowserRouter } from "react-router-dom";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
+import axios from "axios";
 import MainPage from "./Page/MainPage/MainPage";
 import About from "./Page/About/About";
 import Leadership from "./Page/Leadership/Leadership";
 import Board from "./Page/Board/Board";
 import Services from "./Page/Services/Services";
 import Contact from "./Page/Contact/Contact";
+import AdminLogin from "./Page/Admin/AdminLogin";
+
+
+function AuthRedirectRoute()
+{
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try{
+        const response = await axios.post("http://localhost:3000/api/auth/verify-token", {}, { withCredentials: true})
+        setIsAuthenticated(true);
+      }catch(error)
+      {
+        console.log("토큰 인증 실패: ", error);
+        setIsAuthenticated(false);
+      }
+    };
+    verifyToken();
+
+  },[]);
+
+  if(isAuthenticated == null)
+    return null;
+  
+  return isAuthenticated ? <Navigate to="/admin/posts" replace/> : <Outlet/>;
+}
 
 function Layout() {
   return (
@@ -52,6 +80,11 @@ const router = createBrowserRouter([
       }
     ],
   },
+  {
+    path: "/admin",
+    element: <AuthRedirectRoute/>,
+    children: [{ index: true, element: <AdminLogin/>}]
+  }
 ]);
 
 function App() {
---
+++ frontend/src/Page/Admin/AdminPosts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminPosts = () => {
+  return (
+    <div>
+        <div className="container mx-auto max-w-2 shadow-xl shadow-gray-700">
+            <div className="border-black"></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default AdminPosts
